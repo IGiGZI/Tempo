6a2cb745cc402275bae55ef267bf808b2ba2516e
GIGZ <titoadham706@gmail.com>
2026-08-10 08:27:00 +0300

    Added fonts and started styling

--- src/pages/MainCounterPage.jsx
+++ src/pages/MainCounterPage.jsx
@@ -1,8 +1,10 @@
 function MainCounterPage(){
   return <>
-    <div className="container">
-      <div className="counter" >
-        00:00:00:00
+    <div className="flex items-center justify-center min-h-screen text-center">
+      {/* For some unknown reason anything between extrabold and base (400 - 800) shows up weirdly */}
+      <div className="counter text-7xl font-extrabold" >
+        <h1>00:00:00:00</h1>
+        <h1>I am testing the font</h1>
       </div>
     </div>
   </>
